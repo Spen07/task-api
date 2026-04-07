daa75cfaa8b292f3f85e0303fda5e2331983b166
Dieuvit D'Espoir EKONGA <dieuvitekonga22@gmail.com>
2026-04-07 19:27:17 +0200

    feat: add k8s, observability, gitlab-ci and monitoring config

--- src/test/java/sn/isi/l3gl/TaskApiApplicationTests.java
+++ src/test/java/sn/isi/l3gl/TaskApiApplicationTests.java
@@ -1,10 +1,16 @@
 package sn.isi.l3gl;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import sn.isi.l3gl.api.task_api.controller.TaskController;
+import sn.isi.l3gl.core.task_core.service.TaskService;
 
-@SpringBootTest
+@WebMvcTest(TaskController.class)
 class TaskApiApplicationTests {
+
+	@MockBean
+	private TaskService taskService;
 
 	@Test
 	void contextLoads() {
